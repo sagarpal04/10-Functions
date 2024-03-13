927e1686666fa55a3556fd3fed728e6a434fabb2
sagarpal04 <sagarpal0402@gmail.com>
2024-03-13 19:40:15 +0530

    Functions Returning Functions

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 'use strict';
 
-const oneWord = function (str) {
-  return str.replaceAll(' ', '');
-};
-const upperFirstWord = function (str) {
-  const [first, ...other] = str.split(' ');
-  return [first.toUpperCase(), ...other].join(' ');
-};
+const greet = greeting => name => console.log(`${greeting} ${name}`);
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
 
-const transformer = function (str, fn) {
-  console.log(`Original string : ${str}`);
-  console.log(`Transformed string : ${fn(str)}`);
-  console.log(`Transformed by : ${fn.name}`);
-};
-transformer('JavaScript is the best! ', oneWord);
-
-const high5 = function () {
-  console.log('👋');
-};
-document.querySelector('body').addEventListener('click', high5);
-
-['Jonas', 'Smith'].forEach(high5);
+const greeter = greet('Hello');
+greeter('Sagar');
